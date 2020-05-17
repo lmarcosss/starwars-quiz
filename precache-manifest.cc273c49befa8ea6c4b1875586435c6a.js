499456646fc0143f15d27d8d51acc8118d2300d6
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-05-16 21:36:48 -0300

    Updates

---
+++ precache-manifest.cc273c49befa8ea6c4b1875586435c6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10063f48beead07769954b34143c11c2",
+    "url": "/starwars-quiz/index.html"
+  },
+  {
+    "revision": "dc66f578adc3c4506505",
+    "url": "/starwars-quiz/static/js/2.0374b8c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars-quiz/static/js/2.0374b8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9901739550f9f2669ce6",
+    "url": "/starwars-quiz/static/js/main.449e8b24.chunk.js"
+  },
+  {
+    "revision": "78c3b0b5a2a54fa80ac1",
+    "url": "/starwars-quiz/static/js/runtime-main.a53e8ec6.js"
+  },
+  {
+    "revision": "82ef6945ea1491af2a72b06ed18162cd",
+    "url": "/starwars-quiz/static/media/avatar.82ef6945.png"
+  },
+  {
+    "revision": "29eda323edb633876814aa80f12cab84",
+    "url": "/starwars-quiz/static/media/rocket.29eda323.png"
+  }
+]);
